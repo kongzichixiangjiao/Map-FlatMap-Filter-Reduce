5d2722cfe2851ea736cf48cb4275f50aafab7638
kongzichixiangjiao <58895347@qq.com>
2016-07-08 22:34:48 +0800

    Initial Commit

---
+++ Map-FlatMap-Filter-ReduceTests/Map_FlatMap_Filter_ReduceTests.swift
@@ -0,0 +1,36 @@
+//
+//  Map_FlatMap_Filter_ReduceTests.swift
+//  Map-FlatMap-Filter-ReduceTests
+//
+//  Created by houjianan on 16/7/8.
+//  Copyright © 2016年 houjianan. All rights reserved.
+//
+
+import XCTest
+@testable import Map_FlatMap_Filter_Reduce
+
+class Map_FlatMap_Filter_ReduceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
